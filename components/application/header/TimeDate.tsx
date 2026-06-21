3b0bc747d0411f276c0447b448086f5ef0e01d33
[matin bahrami] <evildor33@gmail.com>
2026-06-21 10:30:19 +0330

    chore: setup day.js and time-date component

--- components/application/header/TimeDate.tsx
+++ components/application/header/TimeDate.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 const TimeDate = () => {
-  return <p>🕧 11:11 PM | 31 September 2026</p>;
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return <p>🕧 {currentTime.format("hh:mm A | DD MMMM YYYY")}</p>;
 };
 
 export default TimeDate;
