5938ca36e041692e2e3bd23500f0da55710ca837
[matin bahrami] <evildor33@gmail.com>
2026-06-25 16:38:24 +0330

    style: change application pt from 36px to 28px

--- app/(root)/app/layout.tsx
+++ app/(root)/app/layout.tsx
@@ -5,10 +5,10 @@ import Header from "@/components/application/header/Header";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={`flex bg-background min-h-screen `}>
-      <aside className={`w-73.75 border-r pt-9`}>
+      <aside className={`w-73.75 border-r pt-7`}>
         <Sidebar />
       </aside>
-      <div className={`px-9 pt-9 w-full`}>
+      <div className={`px-9 pt-7 w-full`}>
         <Header />
         <main>{children}</main>
       </div>
---
+++ components/application/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import Cards from "@/components/application/dashboard/Cards";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+};
+
+export default Dashboard;
