54187ce071db66d291ba845dd833fd52c12ba993
[matin bahrami] <evildor33@gmail.com>
2026-07-07 16:50:57 +0330

    feat: Implement add new card action

---
+++ actions/cards/addNewCard.ts
@@ -0,0 +1,59 @@
+"use server";
+import { NewCardSchema, type NewCardSchemaType } from "@/schema/cards";
+import { getSession } from "@/lib/auth-helpers";
+import { db } from "@/db";
+import { eq } from "drizzle-orm";
+import { cards } from "@/db/schema";
+import { revalidatePath } from "next/cache";
+
+export const addNewCard = async (values: NewCardSchemaType) => {
+  const session = await getSession();
+
+  if (!session || !session.user.id) {
+    throw new Error("Unauthorized! Please login again.");
+  }
+
+  const validatedFields = NewCardSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid Fields!",
+    };
+  }
+  const { cardNumber, cardColor, balance, bankName, optionalName, currency } =
+    validatedFields.data;
+
+  const existingCard = await db
+    .select({ id: cards.id })
+    .from(cards)
+    .where(eq(cards.cardNumber, cardNumber))
+    .limit(1);
+
+  if (existingCard.length > 0) {
+    return { error: "You already have a card with this card number." };
+  }
+
+  try {
+    await db.insert(cards).values({
+      cardNumber,
+      userId: session.user.id,
+      cardColor,
+      bankName,
+      balance,
+      customName: optionalName,
+      type: currency,
+    });
+    revalidatePath("/app");
+
+    return { success: "Card added successfully!" };
+  } catch (err) {
+    console.error("DB Insert Error", err);
+
+    return {
+      error:
+        err instanceof Error
+          ? `Database Error ${err.message}`
+          : "An Unexpected database error occurred.",
+    };
+  }
+};
