c8957b3b1c872682de1c613681f49d466a19157b
[matin bahrami] <evildor33@gmail.com>
2026-07-08 16:24:14 +0330

    feat: Add new categories action

---
+++ actions/categories/categories.ts
@@ -0,0 +1,68 @@
+"use server";
+import {
+  createCategorySchema,
+  type CreateCategoryType,
+} from "@/schema/categories";
+import { getSession } from "@/lib/auth-helpers";
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+import { and, eq, ilike } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+export const createCategory = async (values: CreateCategoryType) => {
+  const session = await getSession();
+  if (!session || !session.user.id) {
+    throw new Error("Unathorized");
+  }
+  const { id } = session.user;
+
+  const validatedFields = createCategorySchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields" };
+  }
+
+  const { name, parentId, categoryType, icon } = validatedFields.data;
+
+  try {
+    const [category] = await db
+      .select()
+      .from(categories)
+      .where(and(ilike(categories.name, name), eq(categories.userId, id)))
+      .limit(1);
+
+    if (category) {
+      return {
+        error: "You have a category with same name",
+      };
+    }
+
+    let imageUrl = "";
+    if (icon) {
+      // ُTODO upload image to bucket
+      imageUrl = "https://example.com/path-to-image.png";
+    }
+
+    await db.insert(categories).values({
+      userId: id,
+      name,
+      parentId: parentId ?? null,
+      isDefault: false,
+      icon: imageUrl ?? null,
+      type: categoryType,
+    });
+
+    revalidatePath("/app");
+    return {
+      success: "Category successfully added",
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      error:
+        err instanceof Error
+          ? `Database Error ${err.message}`
+          : "An Unexpected database error occurred.",
+    };
+  }
+};
---
+++ schema/categories.ts
@@ -0,0 +1,39 @@
+import { transactionTypeEnum } from "@/db/schema";
+import * as z from "zod";
+
+const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2MB
+
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+export const createCategorySchema = z.object({
+  categoryType: z.enum(transactionTypeEnum.enumValues),
+  // icon: z.string().max(128, { error: "Too Many Characters" }).trim().optional(),
+  parentId: z.uuid().nullable().optional(),
+  name: z
+    .string()
+    .min(1, "Please enter a name")
+    .max(32, { error: "Too many characters" })
+    .trim(),
+
+  icon: z
+    .preprocess(
+      (val) => (val instanceof FileList ? val[0] : val),
+      z.instanceof(File).optional(),
+    )
+    .refine(
+      (file) => !file || file.size <= MAX_FILE_SIZE,
+      "Max file size is 2MB.",
+    )
+    .refine(
+      (file) => !file || ACCEPTED_IMAGE_TYPES.includes(file.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported.",
+    ),
+});
+
+export type CreateCategoryType = z.input<typeof createCategorySchema>;
+// TODO Figure out what todo with dept id
