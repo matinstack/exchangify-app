69b7937660d2a2a388135a1b7813d2ff4e65c6c9
[matin bahrami] <evildor33@gmail.com>
2026-07-10 19:09:24 +0330

    fix: refactor session checking in header component

--- components/application/header/Header.tsx
+++ components/application/header/Header.tsx
@@ -1,15 +1,14 @@
-"use client";
 import HeaderTitle from "@/components/application/header/HeaderTitle";
 import TimeDate from "@/components/application/header/TimeDate";
 import Notifications from "@/components/application/header/Notifications";
 import UserProfile from "@/components/application/header/UserProfile";
-import { type AuthUser } from "@/types";
+import { getSession } from "@/lib/auth-helpers";
+import { redirect } from "next/navigation";
 
-type HeaderProps = {
-  user: AuthUser;
-};
-const Header = ({ user }: HeaderProps) => {
-  const { name, lastName } = user;
+const Header = async () => {
+  const session = await getSession();
+  if (!session || !session.user.id) redirect("/auth/login");
+  const { name, lastName } = session.user;
   return (
     <header className={"flex justify-between"}>
       <div>
