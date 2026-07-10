10becbaf6339a478089ef9031929f71361c1f1c5
[matin bahrami] <evildor33@gmail.com>
2026-07-10 19:13:32 +0330

    feat: enable cache components and adopt PPR with react suspense

--- app/(root)/app/transactions/page.tsx
+++ app/(root)/app/transactions/page.tsx
@@ -1,26 +1,15 @@
-import Transactions from "@/components/application/transactions/Transactions";
-import { getSession } from "@/lib/auth-helpers";
-import { redirect } from "next/navigation";
-import { getTransactions } from "@/actions/dashboard/dashboard";
 import { type Query } from "@/actions/dashboard/dashboard";
-
+import TransactionComponent from "@/components/application/transactions/TransactionComponent";
+import { Suspense } from "react";
 type Props = {
   searchParams: Promise<Query>;
 };
 
 const transactionsPage = async ({ searchParams }: Props) => {
-  const session = await getSession();
-  if (!session || !session.user.id) {
-    redirect("/login");
-  }
-  const query = await searchParams;
-  const transactions = await getTransactions(query);
-
   return (
-    <Transactions
-      data={transactions.data}
-      pagination={transactions.pagination}
-    />
+    <Suspense fallback={"Loading"}>
+      <TransactionComponent searchParams={searchParams} />
+    </Suspense>
   );
 };
 
---
+++ components/application/transactions/TransactionComponent.tsx
@@ -0,0 +1,26 @@
+import Transactions from "@/components/application/transactions/Transactions";
+import { redirect } from "next/navigation";
+import { getSession } from "@/lib/auth-helpers";
+import { getTransactions, Query } from "@/actions/dashboard/dashboard";
+
+type Props = {
+  searchParams: Promise<Query>;
+};
+
+const TransactionComponent = async ({ searchParams }: Props) => {
+  const session = await getSession();
+  if (!session || !session.user.id) {
+    redirect("/login");
+  }
+  const query = await searchParams;
+  const transactions = await getTransactions(query);
+
+  return (
+    <Transactions
+      data={transactions.data}
+      pagination={transactions.pagination}
+    />
+  );
+};
+
+export default TransactionComponent;
