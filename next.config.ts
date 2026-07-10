1a69013657f084868eeeb0e23d26c6776bcdb48a
[matin bahrami] <evildor33@gmail.com>
2026-07-10 19:09:59 +0330

    chore: setup cacheComponent for project

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  cacheComponents: true,
+
   compress: true,
 
   experimental: {
     optimizePackageImports: ["lucide-react", "@radix-ui/react-icons"],
   },
 
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV === "production",
+  // },
 };
 
 export default nextConfig;
