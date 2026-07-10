d05431d84f3e78ab6e49553664c83d4e17533d2c
[matin bahrami] <evildor33@gmail.com>
2026-07-10 16:38:30 +0330

    fix: transactions page query params

--- app/(root)/app/transactions/page.tsx
+++ app/(root)/app/transactions/page.tsx
@@ -2,13 +2,19 @@ import Transactions from "@/components/application/transactions/Transactions";
 import { getSession } from "@/lib/auth-helpers";
 import { redirect } from "next/navigation";
 import { getTransactions } from "@/actions/dashboard/dashboard";
+import { type Query } from "@/actions/dashboard/dashboard";
 
-const transactionsPage = async () => {
+type Props = {
+  searchParams: Promise<Query>;
+};
+
+const transactionsPage = async ({ searchParams }: Props) => {
   const session = await getSession();
   if (!session || !session.user.id) {
     redirect("/login");
   }
-  const transactions = await getTransactions({});
+  const query = await searchParams;
+  const transactions = await getTransactions(query);
 
   return (
     <Transactions
