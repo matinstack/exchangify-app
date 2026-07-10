3c2f385836e264e6de33e7632a8f1194acdd3150
[matin bahrami] <evildor33@gmail.com>
2026-07-10 19:11:16 +0330

    fix: add cacheLife and cacheTag to getCardsById query

--- data/cards.ts
+++ data/cards.ts
@@ -1,6 +1,12 @@
+import { cacheTag, cacheLife } from "next/cache";
 import { db } from "@/db";
 import { cards } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
-export const getCardsById = async (id: string) =>
-  await db.select().from(cards).where(eq(cards.userId, id));
+export const getCardsById = async (id: string) => {
+  "use cache";
+  cacheLife("days");
+  cacheTag(`cards:${id}`);
+
+  return db.select().from(cards).where(eq(cards.userId, id));
+};
