63c55817591178a329c83b9f948f18a393dab52a
[matin bahrami] <evildor33@gmail.com>
2026-07-17 18:26:15 +0330

    fix: disable prefetch in dn/index.ts

--- db/index.ts
+++ db/index.ts
@@ -4,6 +4,6 @@ import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, { prepare: false });
 
 export const db = drizzle(client, { schema: schema });
