cfa1c65f0ba2ae4add03ee9a97f71738450a9dc7
[matin bahrami] <evildor33@gmail.com>
2026-07-17 13:17:06 +0330

    feat: add logActivity action on register action.

--- actions/auth/Register.ts
+++ actions/auth/Register.ts
@@ -4,6 +4,7 @@ import { getUserByEmail } from "@/data/user";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { logActivity } from "@/lib/log-activity";
 export const RegisterAction = async (values: RegisterSchemaType) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
@@ -22,9 +23,16 @@ export const RegisterAction = async (values: RegisterSchemaType) => {
   }
 
   try {
-    await auth.api.signUpEmail({
+    const res = await auth.api.signUpEmail({
       body: { email, name, lastName, password },
       headers: await headers(),
+    });
+
+    await logActivity({
+      userId: res.user.id,
+      action: "signup",
+      entityType: "user",
+      metadata: { email },
     });
   } catch (err) {
     console.error("REGISTER_ERROR", err);
