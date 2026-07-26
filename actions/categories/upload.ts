6d5f80946c5ad163e33dc858f37586891cbfcfd7
matin <evildor33@gmail.com>
2026-07-26 13:15:30 +0330

    feat: add uploadUrl key and url function

---
+++ actions/categories/upload.ts
@@ -0,0 +1,26 @@
+"use server";
+import { s3 } from "@/lib/s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { imageMetaSchema } from "@/schema/categories";
+import { getSession } from "@/lib/auth-helpers";
+
+export async function getCategoryIconUploudUrl(meta: unknown) {
+  const session = await getSession();
+  if (!session?.user.id) throw new Error("Unauthorized");
+
+  const parsed = imageMetaSchema.safeParse(meta);
+  if (!parsed.success) throw new Error("Unauthorized");
+
+  const { fileName, fileSize, fileType } = parsed.data;
+  const key = `category-icons/${session.user.id}/${crypto.randomUUID()}-${fileName}`;
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.ARVAN_BUCKET!,
+    Key: key,
+    ContentType: fileType,
+    ContentLength: fileSize,
+  });
+  const url = await getSignedUrl(s3, command, { expiresIn: 60 });
+  return { url, key };
+}
