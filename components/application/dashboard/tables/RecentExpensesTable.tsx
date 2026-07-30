09c3d2813690535c5b1d2bc90e55df6382247ca1
matin <evildor33@gmail.com>
2026-07-30 14:30:15 +0330

    style: improve dashboard expense chart layout

--- components/application/dashboard/tables/RecentExpensesTable.tsx
+++ components/application/dashboard/tables/RecentExpensesTable.tsx
@@ -15,126 +15,125 @@ import {
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { MoreVertical } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 function RecentExpensesTable() {
   return (
-    <div
-      className={
-        "bg-card border border-border rounded-lg shadow-xs h-50 overflow-y-auto"
-      }
-    >
-      <h6>test</h6>
-      <Table className="mx-8">
-        <TableHeader>
-          <TableRow>
-            <TableHead>S.N</TableHead>
-            <TableHead>Amount</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead>Sub Category</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
+    <div className={"bg-card border px-8 border-border rounded-lg shadow-xs  "}>
+      <h6 className="text-lg font-semibold py-4">Recent Expenses</h6>
+      <ScrollArea className="overflow-y-auto h-50">
+        <Table className="">
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[10%]">S.N</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Sub Category</TableHead>
+              <TableHead className="w-[5%]">Date</TableHead>
+              <TableHead className="text-right max-w-12">Action</TableHead>
+            </TableRow>
+          </TableHeader>
 
-        <TableBody className={" h-36 overflow-y-auto"}>
-          <TableRow>
-            <TableCell>1.</TableCell>
-            <TableCell>$2100.00</TableCell>
-            <TableCell>Shopping</TableCell>
-            <TableCell>Amazon</TableCell>
-            <TableCell>31 May 2025</TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={"ghost"} size={"icon"}>
-                    <MoreVertical />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem variant="destructive">
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>1.</TableCell>
-            <TableCell>$2100.00</TableCell>
-            <TableCell>Shopping</TableCell>
-            <TableCell>Amazon</TableCell>
-            <TableCell>31 May 2025</TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={"ghost"} size={"icon"}>
-                    <MoreVertical />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem variant="destructive">
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>1.</TableCell>
-            <TableCell>$2100.00</TableCell>
-            <TableCell>Shopping</TableCell>
-            <TableCell>Amazon</TableCell>
-            <TableCell>31 May 2025</TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={"ghost"} size={"icon"}>
-                    <MoreVertical />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem variant="destructive">
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>1.</TableCell>
-            <TableCell>$2100.00</TableCell>
-            <TableCell>Shopping</TableCell>
-            <TableCell>Amazon</TableCell>
-            <TableCell>31 May 2025</TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant={"ghost"} size={"icon"}>
-                    <MoreVertical />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Edit</DropdownMenuItem>
-                  <DropdownMenuItem>Duplicate</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem variant="destructive">
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+          <TableBody className={" h-36 overflow-y-auto"}>
+            <TableRow>
+              <TableCell>1.</TableCell>
+              <TableCell>$2100.00</TableCell>
+              <TableCell>Shopping</TableCell>
+              <TableCell>Amazon</TableCell>
+              <TableCell>31 May 2025</TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                      <MoreVertical />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem variant="destructive">
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>1.</TableCell>
+              <TableCell>$2100.00</TableCell>
+              <TableCell>Shopping</TableCell>
+              <TableCell>Amazon</TableCell>
+              <TableCell>31 May 2025</TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                      <MoreVertical />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem variant="destructive">
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>1.</TableCell>
+              <TableCell>$2100.00</TableCell>
+              <TableCell>Shopping</TableCell>
+              <TableCell>Amazon</TableCell>
+              <TableCell>31 May 2025</TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                      <MoreVertical />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem variant="destructive">
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>1.</TableCell>
+              <TableCell>$2100.00</TableCell>
+              <TableCell>Shopping</TableCell>
+              <TableCell>Amazon</TableCell>
+              <TableCell>31 May 2025</TableCell>
+              <TableCell className="text-right">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant={"ghost"} size={"icon"}>
+                      <MoreVertical />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem variant="destructive">
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </ScrollArea>
     </div>
   );
 }
