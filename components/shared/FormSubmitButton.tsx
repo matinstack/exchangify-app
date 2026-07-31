628409197316522b7ed0e01bb610a968fcd38ff4
[matin bahrami] <evildor33@gmail.com>
2026-07-31 18:28:15 +0330

    feat: FormSubmitButton now accepts props

--- components/shared/FormSubmitButton.tsx
+++ components/shared/FormSubmitButton.tsx
@@ -1,23 +1,27 @@
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 
+type Props = {
+  disabled: boolean;
+  text: string;
+  loadingText: string;
+  className?: string;
+} & React.ComponentPropsWithoutRef<"button">;
+
 const FormSubmitButton = ({
   disabled,
   text,
   loadingText,
   className,
-}: {
-  disabled: boolean;
-  text: string;
-  loadingText: string;
-  className?: string;
-}) => {
+  ...props
+}: Props) => {
   return (
     <Button
       className={`${className}`}
       variant="default"
       type="submit"
       disabled={disabled}
+      {...props}
     >
       {disabled ? `${loadingText}` : `${text}`}
       {disabled && <Spinner />}
