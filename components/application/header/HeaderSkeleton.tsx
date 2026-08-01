9d5afef0c553f1fa777056a2a8bd5463b0b3f51d
[matin bahrami] <evildor33@gmail.com>
2026-08-01 15:49:33 +0330

    style: improve header skleton layout

--- components/application/header/HeaderSkeleton.tsx
+++ components/application/header/HeaderSkeleton.tsx
@@ -1,18 +1,28 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
-const HeaderSkeleton = () => {
+export function HeaderSkeleton() {
   return (
-    <header className={"flex justify-between h-16"}>
-      <div className="space-y-4">
-        <Skeleton className="h-6 w-82 bg-muted" />
-        <Skeleton className="h-4 w-96 bg-muted" />
+    <header className="flex justify-between items-center md:py-7 py-3 px-8 border-b border-border/40">
+      <div>
+        <div className="block md:hidden">
+          <Skeleton className="h-8 w-24 rounded-md" />
+        </div>
+        <div className="hidden md:block md:space-y-2">
+          <Skeleton className="h-7 w-56 rounded-md" />
+          <Skeleton className="h-4 w-96 rounded-md" />
+        </div>
       </div>
-      <div className={" gap-24 "}>
-        <Skeleton className="h-4 w-48" />
-        <Skeleton className="h-4 w-19" />
+      <div className="gap-18 flex items-center">
+        <div className="flex items-center gap- md:mr-4 sm:gap-5">
+          <Skeleton className="h-9 w-9 rounded-full" />
+          <Skeleton className="h-9 w-9 rounded-full" />
+          <div className="ml-2 sm:ml-0 md:hidden">
+            <Skeleton className="h-9 w-9 rounded-md" />
+          </div>
+        </div>
       </div>
     </header>
   );
-};
+}
 
 export default HeaderSkeleton;
