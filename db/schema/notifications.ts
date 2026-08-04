babe27da6b6ba0d16a2874703815d7dfb2c11c83
[matin bahrami] <evildor33@gmail.com>
2026-08-05 00:57:08 +0330

    update: better notifications table

--- db/schema/notifications.ts
+++ db/schema/notifications.ts
@@ -1,13 +1,32 @@
-import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 import { user } from "@/db/schema/auth-schema";
+
+export const notificationTypeEnum = pgEnum("notification_type_enum", [
+  "INFO",
+  "SUCCESS",
+  "WARNING",
+  "ERROR",
+]);
 
 export const notifications = pgTable("notifications", {
   id: uuid("id").primaryKey().defaultRandom(),
   userId: text("user_id")
     .references(() => user.id, { onDelete: "cascade" })
     .notNull(),
-  title: text("title"),
+
+  title: text("title").notNull(),
   message: text("message").notNull(),
-  isRead: boolean("is_read"),
-  createdAt: timestamp("created_at").defaultNow(),
+
+  type: notificationTypeEnum("type").default("INFO").notNull(),
+  link: text("link"),
+
+  isRead: boolean("is_read").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
